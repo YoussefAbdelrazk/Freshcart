bfa6455a0a859699c1ced9d4bc749167c374a0c5
= <youssefabdelrazk2002@gmail.com>
2024-08-16 07:31:05 +0300

    update silder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 
 import { Link } from 'react-router-dom';
-import hero1 from '../assets/hero1.webp';
-import hero2 from '../assets/hero2.webp';
-import hero3 from '../assets/hero3.webp';
-import hero4 from '../assets/hero4.webp';
-const carouselImages = [hero1, hero2, hero3, hero4];
+import hero1 from '../assets/slider-image-1.jpeg'
+import hero2 from '../assets/slider-image-2.jpeg';
+import hero3 from '../assets/slider-image-3.jpeg';
+// import hero4 from '../assets/hero4.webp';
+const carouselImages = [hero1, hero2, hero3];
 export default function Hero() {
   return (
     <div className="grid lg:grid-cols-2 gap-24 items-center">
@@ -29,8 +29,8 @@ export default function Hero() {
           {/* slider */}
           <div className="hidden lg:carousel h-[28rem] carousel-center p-4 space-x-4 bg-neutral rounded-box">
                 {carouselImages.map((image)=>{
-                  return <div key={image} className=' carousel-item'>
-                    <img className=' object-cover rounded-box w-full w-80' src={image} alt="" />
+                  return <div key={image} className=' carousel-item '>
+                    <img className=' rounded-box h-full w-80  object-cover' src={image} alt="" />
                   </div>
                 })}
           </div>
